ce0bb89fcac6f279614706b976ec1901ce6c2075
Ivan Song <mousewurx@gmail.com>
2025-03-08 10:55:26 -0800

    Added Google Analytics 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/main.scss';
 import Header from './components/Header';
@@ -15,11 +17,27 @@ import Mexico from './pages/Mexico';
 import Misc from './pages/Misc';
 import ScrollToTop from './components/ScrollToTop';
 
+const TRACKING_ID = "G-27VVMWXVF0"; // Ensure this ID is correct
+
+function Analytics() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Initialize Google Analytics only once
+    ReactGA.initialize(TRACKING_ID);
+
+    // Track page views
+    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
-      <ScrollToTop />
-      {/* To ensure that the page scrolls to the top when navigating between routes */}
+    <ScrollToTop /> {/* To ensure that the page scrolls to the top when navigating between routes */}
+    <Analytics /> {/* Ensures analytics runs on every page change */}
       <div id="top" className="App app-container">
         <Header />
         <main className="main">
