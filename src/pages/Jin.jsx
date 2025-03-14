4941266c4c2d7f8276e91c91765f2447f432c2e2
Ivan Song <mousewurx@gmail.com>
2025-03-14 10:31:28 -0700

    Added box-shadow property on .sticky-filters class

--- src/pages/Jin.jsx
+++ src/pages/Jin.jsx
@@ -111,7 +111,20 @@ const Jin = () => {
                   <div key={index} className={`col-lg-3 col-md-4 col-sm-6 gallery-item isotope-item ${item.filter}`}>
                     <div className="gallery-wrap">
                       {item.type === 'video' ? (
-                        <div className="video-container">
+                        <div className={`video-container ${isYouTubeVideo ? 'youtube-video' : ''}`}>
+                        {isYouTubeVideo && (
+                          <div className="youtube-logo">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 24 24"
+                              width="24"
+                              height="24"
+                              fill="red"
+                            >
+                              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+                            </svg>
+                          </div>
+                        )}
                           <a
                             href={item.src}
                             data-gallery={item.gallery}
