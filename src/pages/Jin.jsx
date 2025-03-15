3195a8479060f8cb7c557a6ecb36c5222d66f6d8
Ivan Song <mousewurx@gmail.com>
2025-03-15 10:08:50 -0700

    Updated Category 1 to Latin Americe

--- src/pages/Jin.jsx
+++ src/pages/Jin.jsx
@@ -95,11 +95,11 @@ const Jin = () => {
         <div className="container">
           <div className="isotope-layout" data-default-filter=".filter-usa" data-layout="masonry" data-sort="original-order">
             <ul className="gallery-filters isotope-filters sticky-filters" data-aos="fade-up" data-aos-delay="100">
-              <li data-filter=".filter-usa" onClick={() => handleFilterClick('.filter-usa')} className="filter-active">America</li>
+              <li data-filter=".filter-usa" onClick={() => handleFilterClick('.filter-usa')} className="filter-active">USA</li>
               <li data-filter=".filter-korea" onClick={() => handleFilterClick('.filter-korea')}>Korea</li>
+              <li data-filter=".filter-latin-america" onClick={() => handleFilterClick('.filter-latin-america')}>Latin America</li>
               <li data-filter=".filter-category-1" onClick={() => handleFilterClick('.filter-category-1')}>category 1</li>
               <li data-filter=".filter-category-2" onClick={() => handleFilterClick('.filter-category-2')}>category 2</li>
-              <li data-filter=".filter-category-3" onClick={() => handleFilterClick('.filter-category-3')}>category 3</li>
             </ul>
             <div className="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
               {jinItems.map((item, index) => {
